290d72ab77f3edd8f679f32bc2f47a00544e476d
Webcrentebot <66886048+Webcrentebot@users.noreply.github.com>
2020-06-17 01:50:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const Twit = require('twit');
  * - Insert each Twitter credential inside it's equivalent quote.
  */
 const config = {
-  consumer_key: 'process.env.BOT_CONSUMER_KEY',
-  consumer_secret: 'process.env.BOT_CONSUMER_SECRET',
-  access_token: 'process.env.BOT_ACCESS_TOKEN',
-  access_token_secret: 'process.env.BOT_ACCESS_TOKEN_SECRET',
+  consumer_key: process.env.BOT_CONSUMER_KEY,
+  consumer_secret: process.env.BOT_CONSUMER_SECRET,
+  access_token: process.env.BOT_ACCESS_TOKEN,
+  access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET,
 };
 
 /*
