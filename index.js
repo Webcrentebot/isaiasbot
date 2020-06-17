4f8a07f1aad24bc3f25b83a3f911563b8dbac433
Webcrentebot <66886048+Webcrentebot@users.noreply.github.com>
2020-06-17 01:47:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const Twit = require('twit');
  * - Insert each Twitter credential inside it's equivalent quote.
  */
 const config = {
-  consumer_key: 'G9bTjUa7745Iwh7AmA63delq2',
-  consumer_secret: 'TGdp2C6L01Swqlb5hdnOOZev9uMsTQtOZj0LklOqtFUK4NElBb',
-  access_token: '1258984100381294593-QQoFHm8TBA5ZyhXMl4C5wxQlRbqPpC',
-  access_token_secret: 'pinyBGZemPaO8j2aH9pGL1II5m2B1kRU5wHhcSCup4Bgt',
+  consumer_key: 'process.env.BOT_CONSUMER_KEY',
+  consumer_secret: 'process.env.BOT_CONSUMER_SECRET',
+  access_token: 'process.env.BOT_ACCESS_TOKEN',
+  access_token_secret: 'process.env.BOT_ACCESS_TOKEN_SECRET',
 };
 
 /*
